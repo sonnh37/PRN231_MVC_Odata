13faaba1683530766467702af5d1a84ef1e76773
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-10-18 22:33:38 +0700

    init

---
+++ FA24_SE1717_PRN231_G3_KOIORDERINGSYSTEM/KoiOrderingSystem.APIService/Program.cs
@@ -0,0 +1,69 @@
+﻿using KoiOrderingSystem.Data.Models;
+using KoiOrderingSystem.Service;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddOData(options =>
+    {
+        var odataBuilder = new ODataConventionModelBuilder();
+        odataBuilder.EntitySet<Travel>("Travels");
+
+        options.AddRouteComponents("odata", odataBuilder.GetEdmModel())
+               .Select()
+               .Filter()
+               .OrderBy()
+               .Expand()
+               .Count();
+    });
+
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<TravelService>();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
